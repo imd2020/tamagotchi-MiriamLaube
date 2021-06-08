bd4b0f4d0a039420ea7b5bc913bccd2cd8500528
Milucide <73818500+MiriamLaube@users.noreply.github.com>
2021-06-08 13:32:06 +0200

    Create Buttons.js

---
+++ Tamagotchi/Buttons.js
@@ -0,0 +1,28 @@
+export default class Buttons {
+  constructor(x, y, radius) {
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+  }
+  display(picture) {
+    push();
+    imageMode(CENTER);
+    image(picture, this.x, this.y, this.radius, this.radius);
+    pop();
+  }
+  hitTest() {
+    push();
+    if (
+      mouseIsPressed &&
+      mouseX >= this.x - this.radius &&
+      mouseX <= this.x + this.radius &&
+      mouseY >= this.y - this.radius &&
+      mouseY <= this.y + this.radius
+    ) {
+      return true;
+    } else {
+      return false;
+    }
+    pop();
+  }
+}
