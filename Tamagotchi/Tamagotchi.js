892690904970c97cd5c48141d9668e4211a52bb6
Milucide <73818500+MiriamLaube@users.noreply.github.com>
2021-06-08 13:33:06 +0200

    Create Tamagotchi.js

---
+++ Tamagotchi/Tamagotchi.js
@@ -0,0 +1,20 @@
+export default class Tamagotchi {
+  constructor(x, y, width, height, state) {
+    this.x = x;
+    this.y = y;
+    this.height = height;
+    this.width = width;
+    this.state = state;
+  }
+  display(happyBun, sadBun, tiredBun) {
+    push();
+    if (this.state === "happy") {
+      image(happyBun, this.x - 520, this.y - 400, this.width, this.height);
+    } else if (this.state === "sad") {
+      image(sadBun, this.x - 250, this.y - 200, this.width, this.height);
+    } else if (this.state === "tired") {
+      image(tiredBun, this.x - 200, this.y - 200, this.width, this.height);
+    }
+    pop();
+  }
+}
