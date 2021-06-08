b9a5e23d08f0456f380a9a065fa35df0e01d375a
Milucide <73818500+MiriamLaube@users.noreply.github.com>
2021-06-08 13:32:56 +0200

    Create Rooms.js

---
+++ Tamagotchi/Rooms.js
@@ -0,0 +1,33 @@
+export default class Rooms {
+  constructor(x, y, state, width, height) {
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+    this.state = state;
+  }
+  display(
+    kitchenPic,
+    bedroomPic,
+    funroomPic,
+    bathroomPic,
+    cuttingboardPic,
+    drinkingPic
+  ) {
+    push();
+    if (this.state === "kitchen") {
+      image(kitchenPic, this.x, this.y, this.width, this.height);
+    } else if (this.state === "bedroom") {
+      image(bedroomPic, this.x, this.y, this.width, this.height);
+    } else if (this.state === "funroom") {
+      image(funroomPic, this.x, this.y, this.width, this.height);
+    } else if (this.state === "bathroom") {
+      image(bathroomPic, this.x, this.y, this.width, this.height);
+    } else if (this.state === "cuttingboard") {
+      image(cuttingboardPic, this.x, this.y, this.width, this.height);
+    } else if (this.state === "drinking") {
+      image(drinkingPic, this.x, this.y, this.width, this.height);
+    }
+    pop();
+  }
+}
