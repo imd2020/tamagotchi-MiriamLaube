fd84554b72102c8d388d996d3b6529bc169da664
Milucide <73818500+MiriamLaube@users.noreply.github.com>
2021-06-10 14:27:00 +0200

    Update Food.js

--- Tamagotchi/Food.js
+++ Tamagotchi/Food.js
@@ -1,22 +1,56 @@
+angleMode(DEGREES);
 export default class Food {
-  constructor(x, y) {
+  constructor(x, y, xFeed, yFeed, number, stateShow) {
     this.x = x;
     this.y = y;
+    this.xFeed = xFeed;
+    this.yFeed = yFeed;
+    this.stateShow = stateShow;
+    this.foods = [
+      "cherry",
+      "potato",
+      "salad",
+      "cake",
+      "apple",
+      "bread",
+      "nuts",
+      "banana",
+      "tomato",
+      "sushi",
+    ];
+    this.randomNumber = [];
+    this.number = number;
   }
   display(picture) {
-    push();
+    if (this.stateShow === true) {
+      push();
+      translate(mouseX, mouseY);
+      rotate(10);
+      image(picture, -50, -50, 100, 100);
+      pop();
+    }
+  }
+  hitTest() {
     if (
       mouseX >= this.x &&
       mouseX <= this.x + 150 &&
       mouseY >= this.y &&
       mouseY <= this.y + 150
     ) {
-      rotate(100);
-      image(picture, mouseX, mouseY, 0, 0);
+      return true;
+    }
+  }
+  hitTestFeed() {
+    push();
+    if (
+      mouseIsPressed &&
+      mouseX >= this.xFeed &&
+      mouseX <= this.xFeed + 100 &&
+      mouseY >= this.yFeed &&
+      mouseY <= this.yFeed + 125
+    ) {
+      this.stateShow = true;
     }
     pop();
-  }
-  hitTest() {
-    return true;
   }
 }
