1ff75383675a0aed61e0ba8af6a38e9247d52c96
Milucide <73818500+MiriamLaube@users.noreply.github.com>
2021-06-22 22:58:01 +0200

    Update Tamagotchi.js

--- Tamagotchi/Tamagotchi.js
+++ Tamagotchi/Tamagotchi.js
@@ -1,30 +1,31 @@
 export default class Tamagotchi {
-  constructor(x, y, width, height, state) {
+  constructor(x, y, tamagotchiWidth, tamagotchiHeight, state) {
     this.x = x;
     this.y = y;
-    this.height = height;
-    this.width = width;
+    this.tamagotchiHeight = tamagotchiHeight;
+    this.tamagotchiWidth = tamagotchiWidth;
     this.state = state;
   }
   display(happyBun, sadBun, tiredBun) {
     push();
+
     if (this.state === "happy") {
-      image(happyBun, this.x, this.y, this.width, this.height);
+      image(happyBun, this.x, this.y, this.tamagotchiWidth, this.tamagotchiHeight);
     } else if (this.state === "sad") {
       image(
         sadBun,
         this.x - 20,
         this.y + 150,
-        this.width + 60,
-        this.height - 150
+        this.tamagotchiWidth + 60,
+        this.tamagotchiHeight - 150
       );
     } else if (this.state === "tired") {
       image(
         tiredBun,
         this.x + 120,
         this.y + 50,
-        this.width + 200,
-        this.height - 200
+        this.tamagotchiWidth + 200,
+        this.tamagotchiHeight - 200
       );
     }
     pop();
