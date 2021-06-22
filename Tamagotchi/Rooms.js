ad50ae7855282c8e6a6370021444caa24517dac7
Milucide <73818500+MiriamLaube@users.noreply.github.com>
2021-06-22 22:57:49 +0200

    Update Rooms.js

--- Tamagotchi/Rooms.js
+++ Tamagotchi/Rooms.js
@@ -1,9 +1,9 @@
 export default class Rooms {
-  constructor(x, y, state, width, height) {
+  constructor(x, y, state, Roomwidth, Roomheight) {
     this.x = x;
     this.y = y;
-    this.width = width;
-    this.height = height;
+    this.Roomwidth = Roomwidth;
+    this.Roomheight = Roomheight;
     this.state = state;
   }
   display(
@@ -15,19 +15,20 @@ export default class Rooms {
     drinkingPic
   ) {
     push();
+
     if (this.state === "kitchen") {
       //image(pictures["kitchen"]["pic"], this.x, this.y, this.width, this.height);
-      image(kitchenPic, this.x, this.y, this.width, this.height);
+      image(kitchenPic, this.x, this.y, this.Roomwidth, this.Roomheight);
     } else if (this.state === "bedroom") {
-      image(bedroomPic, this.x, this.y, this.width, this.height);
+      image(bedroomPic, this.x, this.y, this.Roomwidth, this.Roomheight);
     } else if (this.state === "funroom") {
-      image(funroomPic, this.x, this.y, this.width, this.height);
+      image(funroomPic, this.x, this.y, this.Roomwidth, this.Roomheight);
     } else if (this.state === "bathroom") {
-      image(bathroomPic, this.x, this.y, this.width, this.height);
+      image(bathroomPic, this.x, this.y, this.Roomwidth, this.Roomheight);
     } else if (this.state === "cuttingboard") {
-      image(cuttingboardPic, this.x, this.y, this.width, this.height);
+      image(cuttingboardPic, this.x, this.y, this.Roomwidth, this.Roomheight);
     } else if (this.state === "drinking") {
-      image(drinkingPic, this.x, this.y, this.width, this.height);
+      image(drinkingPic, this.x, this.y, this.Roomwidth, this.Roomheight);
     }
     pop();
   }
