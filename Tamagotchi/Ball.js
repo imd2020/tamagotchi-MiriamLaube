316c5d87257cb554ad8a5cba49311db657cc6130
Milucide <73818500+MiriamLaube@users.noreply.github.com>
2021-06-24 23:23:51 +0200

    Update Ball.js

--- Tamagotchi/Ball.js
+++ Tamagotchi/Ball.js
@@ -7,19 +7,29 @@ export default class Ball {
     destinationRight,
     desinationLeft,
     stateLeft,
-    rotation
+    rotation,
+    resize
   ) {
-    this.x = x;
-    this.y = y;
-    this.radius = radius;
-    this.destinationRight = destinationRight;
-    this.desinationLeft = desinationLeft;
+    this.resize = resize;
+    this.x = x * this.resize;
+    this.y = y * this.resize;
+    this.radius = radius * this.resize;
+    this.destinationRight = destinationRight * this.resize;
+    this.desinationLeft = desinationLeft * this.resize;
     this.stateLeft = stateLeft;
     this.rotation = rotation;
   }
   display(picture) {
     push();
+    imageMode(CENTER);
+    translate(this.x, this.y);
+    rotate(this.rotation);
+    image(picture, 0, 0, this.radius, this.radius);
+    pop();
+  }
 
+  display(picture) {
+    push();
     imageMode(CENTER);
     translate(this.x, this.y);
     rotate(this.rotation);
@@ -29,7 +39,7 @@ export default class Ball {
 
   left() {
     if (this.x >= this.desinationLeft && this.stateLeft === true) {
-      this.x = this.x - 5;
+      this.x = this.x - 5 * this.resize;
       this.rotation = this.rotation - 10;
       if (this.x === this.desinationLeft) {
         this.stateLeft = false;
@@ -38,7 +48,7 @@ export default class Ball {
   }
   right() {
     if (this.x <= this.destinationRight && this.stateLeft === false) {
-      this.x = this.x + 5;
+      this.x = this.x + 5 * this.resize;
       this.rotation = this.rotation + 10;
       if (this.x === this.destinationRight) {
         this.stateLeft = true;
