2266209e583e12a35aca7a396513c4d6db8903df
Milucide <73818500+MiriamLaube@users.noreply.github.com>
2021-06-24 23:23:43 +0200

    Update Age.js

--- Tamagotchi/Age.js
+++ Tamagotchi/Age.js
@@ -1,16 +1,21 @@
 export default class Age {
-  constructor() {
-    this.x = 815;
-    this.y = 670;
+  constructor(resize) {
+    this.resize = resize;
+    this.x = 815 * this.resize;
+    this.y = 670 * this.resize;
   }
   ageCounter(age) {
-    textSize(24);
+    textSize(24 * this.resize);
     fill(255);
     stroke(0);
-    strokeWeight(4);
-    text("I am " + age + " years old", this.x + 5, this.y);
-    textSize(16);
-    strokeWeight(3.5);
-    text("Try to reach the age of 12!", this.x, this.y + 50);
+    strokeWeight(4 * this.resize);
+    text("I am " + age + " years old", this.x, this.y);
+    textSize(16 * this.resize);
+    strokeWeight(3.5 * this.resize);
+    text(
+      "Try to reach the age of 12!",
+      this.x - 10 * this.resize,
+      this.y + 50 * this.resize
+    );
   }
 }
