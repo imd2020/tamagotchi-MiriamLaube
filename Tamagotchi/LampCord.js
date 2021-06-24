087c75e59ce9b193c40e44950fdf8456ea95ccc4
Milucide <73818500+MiriamLaube@users.noreply.github.com>
2021-06-24 23:24:32 +0200

    Update LampCord.js

--- Tamagotchi/LampCord.js
+++ Tamagotchi/LampCord.js
@@ -1,19 +1,20 @@
 export default class LampCord {
-  constructor(x, y, radius, yMin, yMax, day, xDistance, yDistance) {
-    this.xDistance = xDistance;
-    this.yDistance = yDistance;
-    this.x = x - xDistance;
-    this.y = y - yDistance;
-    this.radius = radius;
-    this.yMin = yMin;
-    this.yMax = yMax;
+  constructor(x, y, radius, yMin, yMax, day, xDistance, yDistance, resize) {
+    this.resize = resize;
+    this.xDistance = xDistance * this.resize;
+    this.yDistance = yDistance * this.resize;
+    this.x = x * this.resize - xDistance * this.resize;
+    this.y = y * this.resize - yDistance * this.resize;
+    this.radius = radius * this.resize;
+    this.yMin = yMin * this.resize;
+    this.yMax = yMax * this.resize;
     this.day = day;
   }
   display(picture, lampshade) {
     push();
 
-    image(picture, this.x, this.y, 0, 0);
-    image(lampshade, 0, 0, 1000, 600);
+    image(picture, this.x, this.y, 2 * this.radius, 220 * this.resize);
+    image(lampshade, 0, 0, 1000 * this.resize, 600 * this.resize);
 
     pop();
   }
@@ -29,7 +30,7 @@ export default class LampCord {
     ) {
       if (mouseY >= this.yMin && mouseY <= this.yMax) {
         this.y = mouseY - this.yDistance;
-        if (mouseY >= this.yMax - 10) {
+        if (mouseY >= this.yMax - 10 * this.resize) {
           this.y = this.yMin - this.yDistance;
           if (this.day === true) {
             this.day = false;
