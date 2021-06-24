c84b7e707f072ac4ba45ae17cc02ec8cb22d7349
Milucide <73818500+MiriamLaube@users.noreply.github.com>
2021-06-24 23:23:59 +0200

    Update Water.js

--- Tamagotchi/Water.js
+++ Tamagotchi/Water.js
@@ -1,24 +1,44 @@
 export default class Water {
-  constructor(x, y, waterHeight, destinationTop, destinationBottom, stateRise) {
-    this.x = x;
-    this.y = y;
-    this.waterHeight = waterHeight;
-    this.destinationTop = destinationTop;
-    this.destinationBottom = destinationBottom;
+  constructor(
+    x,
+    y,
+    waterHeight,
+    destinationTop,
+    destinationBottom,
+    stateRise,
+    resize
+  ) {
+    this.resize = resize;
+    this.x = x * this.resize;
+    this.y = y * this.resize;
+    this.waterHeight = waterHeight * this.resize;
+    this.destinationTop = destinationTop * this.resize;
+    this.destinationBottom = destinationBottom * this.resize;
     this.stateRise = stateRise;
   }
   display() {
     push();
     fill(51, 153, 255);
-    rect(this.x, this.y, 190, this.waterHeight);
-    ellipse(this.x + 95, this.y, 190, 30);
-    ellipse(this.x + 95, 400, 190, 30);
+    noStroke();
+    rect(this.x, this.y, 190 * this.resize, this.waterHeight);
+    ellipse(
+      this.x + 95 * this.resize,
+      this.y,
+      190 * this.resize,
+      30 * this.resize
+    );
+    ellipse(
+      this.x + 95 * this.resize,
+      400 * this.resize,
+      190 * this.resize,
+      30 * this.resize
+    );
     pop();
   }
   rise() {
     if (this.y >= this.destinationTop && this.stateRise === true) {
-      this.y = this.y - 5;
-      this.waterHeight = this.waterHeight + 5;
+      this.y = this.y - 5 * this.resize;
+      this.waterHeight = this.waterHeight + 5 * this.resize;
       if (this.y === this.destinationTop) {
         console.log("ich mach was rise");
         this.stateRise = false;
@@ -27,8 +47,8 @@ export default class Water {
   }
   fall() {
     if (this.y <= this.destinationBottom && this.stateRise === false) {
-      this.y = this.y + 5;
-      this.waterHeight = this.waterHeight - 5;
+      this.y = this.y + 5 * this.resize;
+      this.waterHeight = this.waterHeight - 5 * this.resize;
       if (this.y === this.destinationBottom) {
         console.log("ich mach was fall");
         this.stateRise = true;
@@ -38,10 +58,10 @@ export default class Water {
   hitTest() {
     if (
       mouseIsPressed &&
-      mouseX >= this.x - 10 &&
-      mouseX <= this.x + 200 &&
-      mouseY >= this.destinationTop - 60 &&
-      mouseY <= this.destinationBottom + 50
+      mouseX >= this.x - 10 * this.resize &&
+      mouseX <= this.x + 200 * this.resize &&
+      mouseY >= this.destinationTop - 60 * this.resize &&
+      mouseY <= this.destinationBottom + 50 * this.resize
     ) {
       this.rise();
       this.fall();
