f0f6fb764895fc3fc0097127eda781a9d13e6199
Milucide <73818500+MiriamLaube@users.noreply.github.com>
2021-06-24 23:24:21 +0200

    Update Picture.js

--- Tamagotchi/Picture.js
+++ Tamagotchi/Picture.js
@@ -1,19 +1,20 @@
 export default class Picture {
-  constructor(x, y, waterHeight, waterwidth) {
-    this.x = x;
-    this.y = y;
-    this.waterHeight = waterHeight;
-    this.with = waterwidth;
+  constructor(x, y, waterHeight, waterwidth, resize) {
+    this.resize = resize;
+    this.x = x * this.resize;
+    this.y = y * this.resize;
+    this.waterHeight = waterHeight * this.resize;
+    this.with = waterwidth * this.resize;
   }
   happy(picture) {
     push();
 
     image(
       picture,
-      (this.x = this.x - 250),
-      (this.y = this.y - 250),
-      (this.waterwidth = 520),
-      (this.waterHeight = 400)
+      (this.x = this.x - 250 * this.resize),
+      (this.y = this.y - 250 * this.resize),
+      (this.waterwidth = 520 * this.resize),
+      (this.waterHeight = 400 * this.resize)
     );
     pop();
   }
