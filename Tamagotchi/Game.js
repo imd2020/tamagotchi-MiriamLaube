4316aad4ad635d573d4ec6ed0f70e83ec68d8a6c
Milucide <73818500+MiriamLaube@users.noreply.github.com>
2021-06-25 12:07:38 +0200

    Update Game.js

--- Tamagotchi/Game.js
+++ Tamagotchi/Game.js
@@ -8,35 +8,36 @@ export default class Game {
     this.gameHeight = 600 * this.resize;
     this.sequence = [];
     this.i = 0;
-    this.tutorial1 = loadImage("pictures/tutorial/bunnyTutorual.png");
-    this.tutorial2 = loadImage("pictures/tutorial/buttonTutorial.png");
-    this.tutorial3 = loadImage("pictures/tutorial/livingRoomTutorial.png");
-    this.tutorial4 = loadImage("pictures/tutorial/bedRoomTutorial.png");
-    this.tutorial5 = loadImage("pictures/tutorial/bathRoomTutorial.png");
-    this.tutorial6 = loadImage("pictures/tutorial/kitchenTutorial1.png");
-    this.tutorial7 = loadImage("pictures/tutorial/kitchenTutorial2.png");
-    this.tutorial8 = loadImage("pictures/tutorial/cuttingTutorial.png");
-    this.tutorial9 = loadImage("pictures/tutorial/kitchenTutorial3.png");
-    this.tutorial10 = loadImage("pictures/tutorial/DrinkTutorial.png");
-    this.start = loadImage("pictures/tutorial/playButton.png");
-    this.endhappy = loadImage("pictures/tutorial/youSurvived.png");
-    this.endsad = loadImage("pictures/tutorial/youDiedText.png");
   }
-  display() {
+  display(
+    tutorial1,
+    tutorial2,
+    tutorial3,
+    tutorial4,
+    tutorial5,
+    tutorial6,
+    tutorial7,
+    tutorial8,
+    tutorial9,
+    tutorial10,
+    start,
+    endhappy,
+    endsad
+  ) {
     push();
 
     if (this.state === "tutorial") {
       this.sequence = [
-        this.tutorial1,
-        this.tutorial2,
-        this.tutorial3,
-        this.tutorial4,
-        this.tutorial5,
-        this.tutorial6,
-        this.tutorial7,
-        this.tutorial8,
-        this.tutorial9,
-        this.tutorial10,
+        tutorial1,
+        tutorial2,
+        tutorial3,
+        tutorial4,
+        tutorial5,
+        tutorial6,
+        tutorial7,
+        tutorial8,
+        tutorial9,
+        tutorial10,
       ];
 
       image(
@@ -60,13 +61,13 @@ export default class Game {
     }
 
     if (this.state === "start") {
-      image(this.start, this.x, this.y, this.gameWidth, this.gameHeight);
+      image(start, this.x, this.y, this.gameWidth, this.gameHeight);
     }
     if (this.state === "failed") {
-      image(this.endsad, this.x, this.y, this.gameWidth, this.gameHeight);
+      image(endsad, this.x, this.y, this.gameWidth, this.gameHeight);
     }
     if (this.state === "survived") {
-      image(this.endhappy, this.x, this.y, this.gameWidth, this.gameHeight);
+      image(endhappy, this.x, this.y, this.gameWidth, this.gameHeight);
     }
 
     pop();
